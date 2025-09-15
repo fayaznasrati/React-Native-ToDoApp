8805d21ed700aea2cd1a51abbbbfcd79c9e98f08
fayaz nasrati <fayaz.nasraty@gmail.com>
2025-09-15 14:50:36 +0430

    in this stage we are able to add to do, remove to do form the list, plus it also hundle the Dark and light mood as well

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,98 +1,155 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
-
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
+import { store } from "expo-router/build/global-state/router-store";
+import { useState } from "react";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  useColorScheme
+} from "react-native";
 
 export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
+  const colorScheme = useColorScheme(); // 'light' or 'dark'
 
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+  const styles = createStyles(colorScheme ?? 'light'); // pass mode to functio
+  const [enteredToDo, setEnteredToDo] = useState("");
+
+  const onEnteringToDoHunlder = (enteredText: string) => {
+    setEnteredToDo(enteredText);
+  };
+
+  const [toDoList, setToDoList] = useState<string[]>([]);
+
+  const storeToDoHandler = () => {
+    setToDoList((currentToDoList) => [  enteredToDo, ...currentToDoList ]);
+  };
+
+  const clearToDoHandler = (indexToRemove: number) => {
+    setToDoList((currentToDoList) => 
+      currentToDoList.filter((_, index) => index !== indexToRemove ));
+  };
+  return (
+    <View style={styles.mainContiner}>
+      <View style={styles.ToDoContainer}>
+        <TextInput
+          placeholder="add to do..."
+          placeholderTextColor={colorScheme === "dark" ? "#ccc" : "#888"}
+          style={styles.toDoInputText}
+          onChangeText={onEnteringToDoHunlder}
+          value={enteredToDo}
+        />
+        <TouchableOpacity
+          style={styles.addToDoButton}
+          onPress={() => {
+            storeToDoHandler();
+            setEnteredToDo("");
+          }}
+        >
+          <Text style={styles.addToDoButtonText}>ADD</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.toDoListContainer}>
+        <ScrollView style={styles.toDoLis}>
+          {toDoList.map((toDoItem, index) => (
+            <View key={index} style={styles.toDoItem}>
+              <View>
+                <Text style={styles.toDoItemText}>{toDoItem}</Text>
+              </View>
+              <View>
+                <TouchableOpacity onPress={()=> clearToDoHandler(index)}>
+                  <Text style={styles.removeButton}>X</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          ))}
+        </ScrollView>
+      </View>
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+const createStyles = (mode: string) => StyleSheet.create({
+  mainContiner: {
+    margin: 16,
+    flex: 1,
+    paddingTop: 32,
+    
   },
-  stepContainer: {
-    gap: 8,
+  ToDoContainer: {
+    paddingTop: 16,
+    marginBottom: 16,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  toDoInputText: {
+    boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
+    borderWidth: 1,
+    borderColor: "#aca8a8",
+    backgroundColor: mode === 'dark' ? '#444444' : '#fff',
+    padding: 10,
+    marginBottom: 8,
+    borderRadius: 10,
+    marginRight: 8,
+    width: "80%",
+    height: 50,
+    color: mode === 'dark' ? '#fff' : '#000',
+  },
+  addToDoButton: {
+    boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
+     backgroundColor: mode === 'dark' ? '#444444' : '#2b2b2b',
+    borderRadius: 10,
+    height: 50,
+    justifyContent: "center",
+    width: "20%",
     marginBottom: 8,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  addToDoButtonText: {
+    color: '#ffffff',
+    textAlign: "center",
+    fontWeight: "500",
+  },
+  toDoListContainer: {
+    flex: 1,
+  },
+  toDoLis: {
+    padding: 8,
+    borderWidth: 1,
+    borderRadius: 8,
+    borderColor: mode === 'dark' ? '#555555' : '#ddd',
+
+  },
+  toDoItem: {
+    padding: 12,
+    backgroundColor: mode === 'dark' ? '#444444' : '#fff',
+    borderRadius: 6,
+    boxShadow: "0px 1px 3px rgba(0, 0, 0, 0.1)",
+    marginBottom: 8,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    elevation: 2,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.3,
+    shadowRadius: 1,
+  },
+  toDoItemText: {
+    color: mode === 'dark' ? '#fff' : '#000',
+    fontSize: 16,
+  },
+  removeButton: {
+    color: mode === 'dark' ? '#fff' : '#000',
+    fontSize: 16,
+    padding: 1,
+    borderColor: mode === 'dark' ? '#fff' : '#000',
+    borderWidth: 1,
+    borderRadius: 20,
+    overflow: "hidden",
+    textAlign: "center",
+    paddingLeft: 7,
+    paddingRight: 7,
   },
 });
